e7abd51526a73b8eb1a2a157be520c4b2dcea170
Churrodev720 <dgonzalez720p@gmail.com>
2025-01-26 15:17:09 -0800

    D:

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 import pygame
 import constants
+import player
 
 def main():
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    # Initialize screen WIDTH and HEIGHT
+    SCREEN_WIDTH, SCREEN_HEIGHT = constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Create screen object
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Initialize the display
     print(screen)
     
+    # Create the player at the center of the screen
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
+    my_player = player.Player(x, y)
+    
+    
+    # Game loop setup
     clock = pygame.time.Clock()
     dt = 0
+    
    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+       
+        my_player.update(dt)
+        
         screen.fill((0, 0, 0))
+       
+        my_player.draw(screen)
+        
         pygame.display.flip()
+        
         delta_ms = clock.tick(60)
         dt = delta_ms / 1000
 
