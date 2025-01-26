1120ddd18d8a330eec3ac57d03b3f35c4b727a1a
Churrodev720 <dgonzalez720p@gmail.com>
2025-01-25 19:47:35 -0800

    D:fps

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ def main():
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     print(screen)
+    
+    clock = pygame.time.Clock()
+    dt = 0
+   
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        delta_ms = clock.tick(60)
+        dt = delta_ms / 1000
 
 if __name__ == "__main__":
     main()
